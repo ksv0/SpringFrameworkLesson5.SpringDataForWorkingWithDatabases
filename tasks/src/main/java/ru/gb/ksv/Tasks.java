2d1d77b0c00809b7c7666404c7669310c1326327
ksv0 <128997942+ksv0@users.noreply.github.com>
2024-06-05 11:15:47 +0400

    Урок 11

---
+++ tasks/src/main/java/ru/gb/ksv/Tasks.java
@@ -0,0 +1,15 @@
+package ru.gb.ksv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class Tasks {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tasks.class, args);
+	}
+
+}
